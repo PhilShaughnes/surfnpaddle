997edb052432ea8995f66ffc4a1b7a65cd723acb
Phil <Philip.Shaughnessy@gmail.com>
2017-03-13 23:24:49 -0400

    finished explorer, most of adventure, some of epic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,49 @@
 class UsersController < ApplicationController
-  def index
 
+  before_action :find_user, only: [:show, :edit]
+
+  def index
+    @users = User.order(:name)
   end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def find_user
+    @user = User.find(params[:id]) if params[:id]
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :bio, :photo)
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user, counter_cache: true
-  validates :title, :body, presence: true
+  validates :title, :body, :photo, presence: true
+   validates :title, length: {minimum: 11, :too_short => lambda{ |x, t| " post is #{11 - t[:value].length} too short. Must be more than 10 characters!"}}
 
   default_scope { order(created_at: :desc) }
 
